7a86097144b5ba6d02501cf736ebdffbcd0a881d
Mateus Marwell <marwell.dc@gmail.com>
2021-09-07 01:00:21 -0300

    json movieList's map

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Tmdb from './Tmdb';
+
+/*
+cromograma para criação da pagina.
+
+  Header
+  Destaque
+  Listas
+  rodape basico
+*/
+
 
 export default () => {
+
+  const [movieList, setMovieList ] = useState([]);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      //Pegando a lista total
+      let list = await Tmdb.getHomeList();
+      setMovieList(list)
+    }
+    loadAll();
+  }, []);
+
   return (
-    <div>
-      Olá Mundo!
+    <div className='page'>
+      <section className='lists'>
+        {movieList.map((item, key) => (
+            <div>
+                {item.title}
+            </div>
+        ))}
+      </section>
     </div>
   )
 }
